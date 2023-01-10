e1d35aa79ba4c1ee7de3c82f2a7a1eaf2849c480
sultyl <70979325+sultyl@users.noreply.github.com>
2023-01-10 11:07:13 -0800

    Updates

---
+++ precache-manifest.d833d6d421e715bd9e54807b591c360f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "53d457f53a6bb3e4d74c",
+    "url": "/myportfolio/static/css/main.26176d4e.chunk.css"
+  },
+  {
+    "revision": "53d457f53a6bb3e4d74c",
+    "url": "/myportfolio/static/js/main.53d457f5.chunk.js"
+  },
+  {
+    "revision": "94d0ac11772740e39c35",
+    "url": "/myportfolio/static/js/1.94d0ac11.chunk.js"
+  },
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "48ef1e8cf26d1e71e9f66127584f042c",
+    "url": "/myportfolio/static/media/Ola.48ef1e8c.png"
+  },
+  {
+    "revision": "03f8a775fcad9be0062a1e52a29b00f4",
+    "url": "/myportfolio/static/media/Bus.03f8a775.jpg"
+  },
+  {
+    "revision": "2d8356d7fcb578850b9ef0fd69d2b175",
+    "url": "/myportfolio/static/media/fit.2d8356d7.jpg"
+  },
+  {
+    "revision": "698c63ccdb9e5768a5d5b99824c73551",
+    "url": "/myportfolio/static/media/Por.698c63cc.jpg"
+  },
+  {
+    "revision": "6fe3d6997d02b5ab35bb5444abd09908",
+    "url": "/myportfolio/static/media/Res.6fe3d699.jpg"
+  },
+  {
+    "revision": "02d1539d1633609e38794a6414b5b3a0",
+    "url": "/myportfolio/static/media/Job.02d1539d.jpg"
+  },
+  {
+    "revision": "5f1b75c71ed24ef213a270113bf14026",
+    "url": "/myportfolio/index.html"
+  }
+];
